dc9f8a6e8284d500f707743530f0920e02618e28
Jyotirmoy <jyotirmoykarmakar02@gmail.com>
2023-06-02 22:15:17 +0530

    logo

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -10,11 +10,21 @@ type LogoProps = {
 const Logo = ({ className }: LogoProps) => (
   <Link href='/'>
     <div className={cn(styles.logo, className)}>
+      <div
+        style={{
+          position: "absolute",
+          left: "25px",
+          top: "9px",
+        }}
+      >
+        <Image src='/images/triangles.svg' width='100' height='50' alt='logo' />
+      </div>
       <Image
-        src='/images/gainxlogo.png'
-        width='200'
-        height='100'
-        alt='AstroClash'
+        src='/images/logo.svg'
+        width='150'
+        height='70'
+        alt='logo'
+        style={{ opacity: "0.5", marginTop: "30px" }}
       />
     </div>
   </Link>
